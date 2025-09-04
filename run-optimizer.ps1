718f94d0ebcc2bb0906f8dd3b0a5ba28d6f5f1ef
clickinformaticavillaramallo <clickinformaticavillaramallo@gmail.com>
2025-09-04 18:03:43 -0300

    Update run-optimizer.ps1

--- run-optimizer.ps1
+++ run-optimizer.ps1
@@ -1,5 +1,5 @@
 # run-optimizer.ps1
-$exeUrl = "https://github.com/hellzerg/optimizer/releases/latest/download/Optimizer.exe"
+$exeUrl = "https://github.com/hellzerg/optimizer/releases/download/16.9/Optimizer.exe"
 $tmpExe = "$env:TEMP\Optimizer.exe"
 
 Write-Host "[*] Descargando Optimizer..." -ForegroundColor Yellow
