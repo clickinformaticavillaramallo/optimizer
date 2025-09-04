bc501493fe88242ecabc03a3b4ebd79c2866ffd5
clickinformaticavillaramallo <clickinformaticavillaramallo@gmail.com>
2025-09-04 18:02:15 -0300

    Add files via upload

---
+++ run-optimizer.ps1
@@ -0,0 +1,9 @@
+# run-optimizer.ps1
+$exeUrl = "https://github.com/hellzerg/optimizer/releases/latest/download/Optimizer.exe"
+$tmpExe = "$env:TEMP\Optimizer.exe"
+
+Write-Host "[*] Descargando Optimizer..." -ForegroundColor Yellow
+Invoke-WebRequest -Uri $exeUrl -OutFile $tmpExe -UseBasicParsing
+
+Unblock-File -Path $tmpExe
+Start-Process $tmpExe -Wait
